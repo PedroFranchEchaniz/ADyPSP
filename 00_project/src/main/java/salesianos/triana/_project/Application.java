bc8ca57c01a760df69fef348a3db0f9ac26fa75c
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-09-25 10:47:38 +0200

    cosas

---
+++ 00_project/src/main/java/salesianos/triana/_project/Application.java
@@ -0,0 +1,13 @@
+package salesianos.triana._project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
